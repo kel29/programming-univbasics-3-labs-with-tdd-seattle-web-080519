b5c37635b9afca68a34a15650f89b6e8bad49722
Krista <krista.loven.87@gmail.com>
2019-06-30 23:42:40 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,4 +1,6 @@
 # Add your variables here
 def calculator(first_number, second_number)
   sum = first_number + second_number
+  difference = first_number - second_number
+  product = first_number * second_number
   
