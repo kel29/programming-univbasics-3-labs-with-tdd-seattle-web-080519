0196e42671214d5c93e4922a0bc1b400350cefdc
Krista <krista.loven.87@gmail.com>
2019-06-30 23:44:53 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,7 +1,9 @@
 # Add your variables here
-def calculator(first_number, second_number)
-  sum = first_number + second_number
-  difference = first_number - second_number
-  product = first_number * second_number
-  quotient = first_number / second_number
-end 
+
+first_number = 3
+second_number = 5
+
+sum = first_number + second_number
+difference = first_number - second_number
+product = first_number * second_number
+quotient = first_number / second_number
